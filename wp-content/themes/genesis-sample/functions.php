03f619968d91491a1a5509c02a258290a5ad130e
Card Advisor Hub <info@cardadvisorhub.com>
2025-08-06 03:34:23 +0530

    Customize Genesis Sample theme: add top menu in title bar with custom header and menu location

--- wp-content/themes/genesis-sample/functions.php
+++ wp-content/themes/genesis-sample/functions.php
@@ -1,3 +1,16 @@
+// Register a top menu location
+function genesis_sample_register_top_menu() {
+    register_nav_menu('top-menu', __('Top Menu', 'genesis-sample'));
+}
+add_action('after_setup_theme', 'genesis_sample_register_top_menu');
+
+// Override the default Genesis header with our custom header
+remove_action('genesis_header', 'genesis_do_header');
+add_action('genesis_header', 'genesis_sample_custom_header');
+
+function genesis_sample_custom_header() {
+    get_template_part('header');
+}
 <?php
 /**
  * Genesis Sample Theme Functions - Minimal Version
